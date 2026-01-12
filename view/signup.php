7f19cf8cbc171b3e6628e090b89af50633865497
HP <eshachwdhry@gmail.com>
2026-01-12 12:11:43 +0600

    user inputs

--- view/signup.php
+++ view/signup.php
@@ -8,4 +8,13 @@
         <h2>Create Account</h2>
         <form method="post" action="">
             <input type="text" name="username" placeholder="Username"><br><br>
-            
+            <input type="password" name="password" placeholder="Password"><br><br>
+            <input type="text" name="email" placeholder="Email"><br><br>
+            <input type="submit" value="Sign Up" class="btn">
+</form>
+<div class="message">
+    <?php if(!empty($message))echo $message;?>
+</div>
+</div>
+</body>
+</html>
