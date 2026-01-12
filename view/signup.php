30e5c63879a81f20aca39e35b59ecd258de035da
HP <eshachwdhry@gmail.com>
2026-01-12 11:42:51 +0600

    signup page

---
+++ view/signup.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Sign Up</title>
+</head>
+<body>
+    <div class="container">
+        <h2>Create Account</h2>
+        <form method="post" action="">
+            <input type="text" name="username" placeholder="Username"><br><br>
+            
