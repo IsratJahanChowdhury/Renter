4d95f16a951a6e7bbbc65957f5ff7e988d0141cf
HP <eshachwdhry@gmail.com>
2026-01-18 01:30:24 +0600

    login input field

--- view/login.php
+++ view/login.php
@@ -1,4 +1,21 @@
 <!DOCTYPE html>
 <html>
-    
-</html>
+    <head>
+        <title>Login</login>
+    </head>
+<body>
+    <div class="login-box">
+        <h2>&#128274;Login</h2>
+        <p>Welcome back! Please login</p>
+        <form method="post">
+        <div class="input-group">
+        <i>&#128100;</i>
+        <input type=text name="username" placeholder="Username" value="<?php echo $username; ?>">
+    </div>
+    <div class="input-group">
+        <i>&#128273;</i>
+        <input type="password" name="password" id="password"placeholder="Password">
+    </div>
+    <div class="show-pass" onclick="togglePassword()">Show Password</div>
+</body>
+ </html>
