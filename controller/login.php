00b2587962053f3bd677db20c8513f103cee4e0a
HP <eshachwdhry@gmail.com>
2026-01-18 03:10:32 +0600

    if matches then goes to dashboard

--- controller/login.php
+++ controller/login.php
@@ -8,5 +8,17 @@ $Error="";
 if(!isset($_SESSION["username"])&& isset($_COOKIE["username"])){
     $cookieUser=$_COOKIE["username"];
     $sql= "SELECT * FROM users WHERE username='$cookieUser'";
+    $result=mysqli_query($conn,$sql);
     
+    if(mysqli_num_rows($result)==1)
+        {
+            $_SESSION["username"]=$cookieUser;
+            header("Location:dashboard.php");
+            exit();
+        }
+        else
+        {
+          setcookie("username", "", time()-3600 ,"/");
+        }
+
 }
