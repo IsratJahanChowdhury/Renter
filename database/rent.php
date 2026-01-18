c0573a909b4621ebc79af5620aaf06f6f0834f5d
HP <eshachwdhry@gmail.com>
2026-01-18 10:30:01 +0600

    fixed error

--- database/rent.php
+++ database/rent.php
@@ -3,7 +3,7 @@ $host="localhost";
 $user="root";
 $pass="";
 $dbname = "rent";
-$conn = new mysqli($host, $user, $pass, $dbname)
+$conn = new mysqli($host, $user, $pass, $dbname);
 
 if($conn->connect_error)
     {
