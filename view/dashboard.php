53ab973e1f89010812f3a888200ba457e331f03c
HP <eshachwdhry@gmail.com>
2026-01-18 17:38:59 +0600

    added header, book item container

--- view/dashboard.php
+++ view/dashboard.php
@@ -1,10 +1,34 @@
 <?php include("../controller/dashboard.php");?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Renter Dashboard</title>
-        <link rel="stylesheet" href="../stylesheets/dashboard.css">
+ <head>
+     <title>Renter Dashboard</title>
+     <link rel="stylesheet" href="../stylesheets/dashboard.css">
 </head>
 <body>
+    <a href="../view/home.html" class="home-btn">Home</a>
+
+    <div class="title">
+    <h2>EasyRent</h2>
+    <p>Browse,book, and enjoy your rentals!</p>
+    </div>
+
+    <div class="header">
+    Welcome,<?php echo $displayName;?>! Ready to manage your rentals?
+    </div>
+
+    <div class="container">
+        
+    <div class="card">
+    <div clsss="icon">&#128230;</div>
+    <h3>Book item</h3>
+    <p>Rent an available item</p>
+    </div>
+
+
+
+
+
+
 </body>
 </html>
