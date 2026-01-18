a9315adf97d3929d3d706142cb0825697dc8c6e5
HP <eshachwdhry@gmail.com>
2026-01-18 17:27:31 +0600

    dashboard html

---
+++ view/dashboard.php
@@ -0,0 +1,10 @@
+<?php include("../controller/dashboard.php");?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Renter Dashboard</title>
+        <link rel="stylesheet" href="../stylesheets/dashboard.css">
+</head>
+<body>
+</body>
+</html>
