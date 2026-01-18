00b5785220b1a07163e64593c45f5ced5b6ea382
HP <eshachwdhry@gmail.com>
2026-01-18 12:37:40 +0600

    empty field validation

--- controller/signup.php
+++ controller/signup.php
@@ -1,16 +1,15 @@
 <?php 
+session();
+include("../database/rent.php");
 $message = "";
 if($_SERVER["REQUEST_METHOD"]== "POST")
 {
-    $username =$_POST["username"];
+    $username =trim($_POST["username"]);
     $password=$_POST["password"];
-    $email=$_POST["email"];
+    $email=trim($_POST["email"]);
      if(empty($username)|| empty($password)|| empty($email)){
-    $message = "<p class='error'> All fields are required</p>;
+    $message = "<p class='error'> All fields are required</p>";
      }
-    else{
-        $message = "<p class='success'> Account created successfully!</p>";
-    $message="<a href='../view/dashboard.php'>Go to Dashboard </a>;
- }
-}
-?>
+    
+        }
+        ?>
