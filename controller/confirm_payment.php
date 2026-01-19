513c8d120176ce9ccc2baf54444ff0942a84c3ea
HP <eshachwdhry@gmail.com>
2026-01-19 21:48:36 +0600

    zz

--- controller/confirm_payment.php
+++ controller/confirm_payment.php
@@ -6,6 +6,7 @@ if(!isset($_SESSION['booking']))
         header("Location:../view/payment.php");
         exit();
     }
+    
     if(isset($_SESSION['paid']))
         {
             header("Location:../view/payment.php");
@@ -13,5 +14,16 @@ if(!isset($_SESSION['booking']))
         }
         $booking=$_SESSION['booking'];
         $item_id=$booking['item_id'];
-        $sql="UPDATE items SET quantity = quantity-1 WHERE id='$item_id AND quantity >0";
+        $sql="UPDATE items SET quantity = quantity-1 WHERE id='$item_id' AND quantity >0";
+        $result=mysqli_query($conn,$sql);
         
+        if(!$result || mysqli_affected_rows($conn)==0)
+            {
+                die("Item out of stock");
+            }
+
+        $_SESSION['paid']=true;
+        unset($_SESSION['booking']);
+        header("Location:../view/payment.php?success=1");
+        exit();
+        ?>
