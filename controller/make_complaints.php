b43d6af8cc2eeee8e6621cfb289bb693fb5edf4a
HP <eshachwdhry@gmail.com>
2026-01-19 16:09:33 +0600

    complaint page html

---
+++ controller/make_complaints.php
@@ -0,0 +1,7 @@
+<?php
+include("../database/rent.php");
+$message=$_POST['message'];
+mysqli_query($conn,"INSERT INTO complaints(message)VALUES('message')"
+);
+header("Location:../view/make_complaint.php");
+?>
--- view/dashboard.php
+++ view/dashboard.php
@@ -16,49 +16,47 @@
     <div class="header">
     Welcome,<?php echo $displayName;?>! Ready to manage your rentals?
     </div>
-    <a href="../view/book.php"class="card-link">
+
     <div class="container">
+    <a href="../view/book.php"class="card-link">
     <div class="card">
     <div class="icon">&#128230;</div>
     <h3>Book item</h3>
     <p>Rent an available item</p>
     </div>
+    </a>
 
-    <div class="card">
-    <div class="icon">&#128269;</div>
-    <h3>Search</h3>
-    <p>Find items by name</p>
-    </div>
-
+    
+    <a href="../view/cancel_booking.php"class="card-link">
     <div class="card">
     <div class="icon">&#10060;</div>
     <h3>Cancel Booking</h3>
     <p>Cancel your booking</p>
     </div>
+    </a>
      
+    <a href="../view/make_complaint.php"class="card-link">
     <div class="card">
     <div class="icon">&#9997;</div>
     <h3>Make Complaint</h3>
     <p>Report an issue</p>
     </div>
     
+    <a href="../view/request_refund.php"class="card-link">
     <div class="card">
     <div class="icon">&#128184;</div>
     <h3>Request Refund</h3>
     <p>Ask for Refund</p>
     </div>
-
-    <div class="card">
-    <div class="icon">&#128179;</div>
-    <h3>Make Payment</h3>
-    <p>Pay rental charges</p>
-    </div>
-
+    </a>
+    
+    <a href="../view/submit_review.php"class="card-link">
     <div class="card">
     <div class="icon">&#11088;</div>
     <h3>Submit Review</h3>
     <p>Rate and review item</p>
     </div>
+    </a>
     </div>
     <a href="../controller/logout.php"class="logout-btn">Logout</a>
 </body>
