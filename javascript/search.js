6d50edc68bab7696a262ea4cafaefb216c304db7
HP <eshachwdhry@gmail.com>
2026-01-19 12:51:24 +0600

    using Ajax

---
+++ javascript/search.js
@@ -0,0 +1,15 @@
+function searchItem()
+{
+    var q = document.getElementById("searchBox").ariaValueMax;
+
+}
+var xhttp=new XMLHttpRequest();
+xhttp.onreadystatechange=function()
+{
+    if(this.readyState===4 && this.state===200)
+    {
+        var data= JSON.parse(this.responseText);
+        var html="";
+        
+    }
+}
