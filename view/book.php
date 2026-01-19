bca1133ed494fad0bd0b3a370f3776c1434ac2f6
HP <eshachwdhry@gmail.com>
2026-01-19 09:29:49 +0600

    search field, back button and basic html

--- view/book.php
+++ view/book.php
@@ -10,3 +10,17 @@ if(isset($_GET['q'])&&$_GET['q']!="")
             $result=mysqli_query($conn,"SELECT*FROM items");
         }
         ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Book items</title>
+        <link rel="stylesheet" href="../css/book.css">
+    </head>
+    <body>
+        <a href="dashboard.php" class="back-btn">Back</a>
+        <div class="page-box">
+
+        <form method="get">
+        <input type="text" name="q" placeholder="Search item">
+        <button type="submit">&#128269;Search</button>
+        </form>
