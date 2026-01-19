8dc1a4736e648eaad5bb19fd06a6bf3606738b01
HP <eshachwdhry@gmail.com>
2026-01-19 13:01:31 +0600

    image,price,availibility

--- javascript/search.js
+++ javascript/search.js
@@ -10,6 +10,19 @@ xhttp.onreadystatechange=function()
     {
         var data= JSON.parse(this.responseText);
         var html="";
-        
+        if(data.length===0)
+        {
+            html="<p class'not-available'>No product found</p>";
+        }
+        for(var i=0;i<data.length;i++)
+        {
+            html+=`
+            <div class="item-box">
+            <img src="../images/${data[i].image}"alt="item">
+            <h3>${data[i].name}</h3>
+            <p>Price:Tk ${data[i].price}</p>
+            <p>Available:${data[i].quantity}</p>
+            
+        }
     }
 }
