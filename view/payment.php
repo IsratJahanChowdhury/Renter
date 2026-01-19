02288703ef3663b6c6ee6ad82dea4c23ecfdb7f2
HP <eshachwdhry@gmail.com>
2026-01-19 19:08:44 +0600

    adding radio button to select payment method

---
+++ view/payment.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Payment</title>
+</head>
+<body>
+    <div class ="page-box">
+        <h2>Select Payment ethod</h2>
+        <form method ="post" action ="../controller/confirm_payment.php">
+            <label><input type="radio" name="method" value="Card" required>Card</label>
+             <label><input type="radio" name="method" value="Bkash">Bkash</label><br>
+             <label><input type="radio" name="method" value="Cash">Bkash</label><br><br>
+             
+            
