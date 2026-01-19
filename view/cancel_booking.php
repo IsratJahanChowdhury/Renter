2586128237c636fab5215801f8bd3f1f8b2dc22c
HP <eshachwdhry@gmail.com>
2026-01-19 17:14:23 +0600

    error message and back button

--- view/cancel_booking.php
+++ view/cancel_booking.php
@@ -5,12 +5,24 @@
         <link rel="stylesheet" href="../stylesheets/book.css">
 </head>
 <body>
+    <a href="dashboard.php" class="back-btn">Back</a>
     <div class="page-box">
         <h2>Cancel Booking</h2>
         <form method = "post" action ="../controller/cancel_booking.php">
             <input type = "text" name="booking_id" placeholder="Booking Id" required>
             <button type="submit">Cancel Booking</button>
-</form>
+        </form>
+            <?php
+             if(isset($_GET['error']))
+            {
+           echo '<p class="not-available">Invalid Booking ID</p>';
+            }
+            
+            if(isset($_GET['success']))
+            {
+             echo '<p class="message">Booking cancelled successfully</p>';
+            }
+             ?>     
 </div>
 </body>
 </html>
