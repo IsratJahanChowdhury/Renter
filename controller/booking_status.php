25b66848aac5f22f8295aea72209bd4ab8c9f3d4
HP <eshachwdhry@gmail.com>
2026-01-19 18:21:13 +0600

    validation for booking status

---
+++ controller/booking_status.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+if(!isset($_SESSION['booking']))
+    {
+header("Location:../view/book.php");
+exit();
+    }
+    $booking = $_SESSION['booking'];
+    include("../view/booking_status.php");
