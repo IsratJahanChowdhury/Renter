194f60ba8ba2f25c1a22785b8b71c7645cd89723
HP <eshachwdhry@gmail.com>
2026-01-19 19:00:10 +0600

    syntax issues

--- view/booking_status.php
+++ view/booking_status.php
@@ -2,13 +2,21 @@
 <html>
 <head>
     <title>Booking Status</title>
+    <link rel="stylesheet" href="../stylesheets/book.css">
 </head>
 <body>
     <div class="page-box">
         <h2>Booking Details</h2>
         <div class="item-box">
         <img src="../images/<?php echo $booking['image'];?>">
-        <h3><?php echo $booking['name'];?></p>
+        <h3><?php echo $booking['name'];?></h3>
         <p>Days:<?php echo $booking['days'];?></p>
         <p>Total: Tk <?php echo $booking['total'];?></p>
-        
+
+        <form action= "../controller/payment.php" method="post">
+        <button type="submit" >Make Payment </button>
+        </form>
+</div>
+</div>
+</body>
+</html>
