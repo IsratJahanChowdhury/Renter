332ec26ca329eb38da703af0ecad4466af9fab5d
HP <eshachwdhry@gmail.com>
2026-01-19 09:56:45 +0600

    date,submit,not available message

--- view/book.php
+++ view/book.php
@@ -37,5 +37,41 @@ if(isset($_GET['q'])&&$_GET['q']!="")
                         <h3><?php echo $row['name'];?> </h3>
                         <p>Price:Tk<?php echo $row['price'];?></p>
                         <p>Available:<?php echo $row['quantity'];?></p>
-                    }
-            }
+                        <?php if($row['quantity']>0)
+                        {
+                            ?>
+                            <form method="post"action="../control/book.php">
+                            input type="hidden"name="item_id" value="<?php echo $row['id'];?>">
+                            <label>From date</label>
+                            <input type="date" name="from_date"required>
+                            <label>To Date</label>
+                            <input type="date" name= "to_date" required>
+                            <button type="submit">Book Item</button>
+                            </form>
+                            <?php
+                        }
+                        else
+                            {
+                                ?>
+                                <center>
+                                    <p class="not-available">Product is not available right now</p>
+                                </center>
+                                <?php
+                            }
+                            ?>
+                            </div>
+                            <?php
+                        }
+                        }
+                        else
+                            {
+                                echo"<p class='not-available'>No Product found</p>";
+
+                            }
+                            ?>
+                            </div>
+</div>
+</body>
+</html>
+
+    
