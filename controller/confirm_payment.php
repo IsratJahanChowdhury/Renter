7cb626b13b50c7ded77531c574d2fa6fa8507fcd
HP <eshachwdhry@gmail.com>
2026-01-19 20:50:14 +0600

    logics to reduce availibility after booking

--- controller/confirm_payment.php
+++ controller/confirm_payment.php
@@ -1,16 +1,17 @@
 <?php
 session_start();
 include("../database/rent.php");
-if(isset($_SESSION['booking']))
+if(!isset($_SESSION['booking']))
     {
-        header("Location:../view/book.php");
+        header("Location:../view/payment.php");
         exit();
     }
-    $booking=$_SESSION['booking'];
-    $item_id=$booking['item_id'];
-
-    mysql_query($conn, "UPDATE items SET quantity = quantity- 1 WHERE id='$item_id'");
-    unset($_SESSION['booking']);
-        $_SESSION['success']="Payment successfull!Booking Confirmed.";
-   header("Location:../view/payment.php?success==1");
-     exit();
+    if(isset($_SESSION['paid']))
+        {
+            header("Location:../view/payment.php");
+            exit();
+        }
+        $booking=$_SESSION['booking'];
+        $item_id=$booking['item_id'];
+        $sql="UPDATE items SET quantity = quantity-1 WHERE id='$item_id AND quantity >0";
+        
