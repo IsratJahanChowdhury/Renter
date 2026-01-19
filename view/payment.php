93ec26e5a0fd621d7e987cf3443004dbeb21c93f
HP <eshachwdhry@gmail.com>
2026-01-19 21:49:46 +0600

    s

--- view/payment.php
+++ view/payment.php
@@ -2,13 +2,27 @@
 <html>
     <head>
         <title>Payment</title>
+        <link rel="stylesheet" href="../stylesheets/book.css">
 </head>
 <body>
+    <a href="book.php" class="back-btn">Back</a>
     <div class ="page-box">
-        <h2>Select Payment ethod</h2>
+        <h2>Select Payment Method</h2>
         <form method ="post" action ="../controller/confirm_payment.php">
             <label><input type="radio" name="method" value="Card" required>Card</label>
              <label><input type="radio" name="method" value="Bkash">Bkash</label><br>
-             <label><input type="radio" name="method" value="Cash">Bkash</label><br><br>
+             <label><input type="radio" name="method" value="Cash">Cash</label><br><br>
+             <button type= "submit">Pay Now</button>
+</form>
+<?php
+if(isset($_GET['success'])&& $_GET['success']==1)
+            {
+             echo '<p class="message">Payment successfully! Booking Confirmed</p>';
+            }
+             
+    ?>
+</div>
+</body>
+</html>
              
             
