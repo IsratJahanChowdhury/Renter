7db7243c8abd8f15aef3b5246f93575c0c63bd0e
HP <eshachwdhry@gmail.com>
2026-01-19 18:14:22 +0600

    used associative array to store details

--- controller/book.php
+++ controller/book.php
@@ -9,4 +9,16 @@ if($_SERVER["REQUEST_METHOD"]=="POST")
     $item = mysqli_fetch_assoc($result);
      
     $days = (strtotime($to)- strtotime($from)) / (60*60*24);
-    
+     if($days <=0) days=1;
+     $_SESSION['booking']=
+     [
+        'item_id'=> $item['id'],
+        'name'=> $item['name'],
+        'image'=>$item['image'],
+        'price'=> $item['price'],
+        'days'=> $days,
+        'total' => $days * $item['price']
+     ];
+     header("Location:booking_status.php");
+     exit();
+    }
