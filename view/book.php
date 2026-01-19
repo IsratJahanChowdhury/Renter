769f73250dde0c508b342283c33fb528de3e4c92
HP <eshachwdhry@gmail.com>
2026-01-19 10:21:40 +0600

    wrong path

--- view/book.php
+++ view/book.php
@@ -14,7 +14,7 @@ if(isset($_GET['q'])&&$_GET['q']!="")
 <html>
     <head>
         <title>Book items</title>
-        <link rel="stylesheet" href="../css/book.css">
+        <link rel="stylesheet" href="../stylesheets/book.css">
     </head>
     <body>
         <a href="dashboard.php" class="back-btn">Back</a>
@@ -38,38 +38,38 @@ if(isset($_GET['q'])&&$_GET['q']!="")
                         <p>Price:Tk<?php echo $row['price'];?></p>
                         <p>Available:<?php echo $row['quantity'];?></p>
                         <?php if($row['quantity']>0)
-                        {
-                            ?>
-                            <form method="post"action="../control/book.php">
-                            input type="hidden"name="item_id" value="<?php echo $row['id'];?>">
-                            <label>From date</label>
-                            <input type="date" name="from_date"required>
-                            <label>To Date</label>
-                            <input type="date" name= "to_date" required>
-                            <button type="submit">Book Item</button>
-                            </form>
-                            <?php
-                        }
-                        else
-                            {
-                                ?>
-                                <center>
-                                    <p class="not-available">Product is not available right now</p>
-                                </center>
-                                <?php
-                            }
-                            ?>
-                            </div>
-                            <?php
-                        }
-                        }
-                        else
-                            {
-                                echo"<p class='not-available'>No Product found</p>";
+                    {
+                        ?>
+                        <form method="post"action="../control/book.php">
+                        input type="hidden"name="item_id" value="<?php echo $row['id'];?>">
+                        <label>From date</label>
+                        <input type="date" name="from_date"required>
+                        <label>To Date</label>
+                        <input type="date" name= "to_date" required>
+                        <button type="submit">Book Item</button>
+                        </form>
+                        <?php
+                    }
+                else
+                    {
+                        ?>
+                        <center>
+                            <p class="not-available">Product is not available right now</p>
+                        </center>
+                        <?php
+                    }
+                        ?>
+                        </div>
+                        <?php
+                    }
+                    }
+                else
+                    {
+                         echo"<p class='not-available'>No Product found</p>";
 
-                            }
-                            ?>
-                            </div>
+                    }
+                        ?>
+                        </div>
 </div>
 </body>
 </html>
