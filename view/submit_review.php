c22af619d3988dc5b26fe7e4114b8e7bee23f201
HP <eshachwdhry@gmail.com>
2026-01-19 18:22:02 +0600

    success message

--- view/submit_review.php
+++ view/submit_review.php
@@ -5,6 +5,7 @@
         <link rel="stylesheet"href="../stylesheets/book.css">
 </head>
 </body>
+ <a href="dashboard.php" class="back-btn">Back</a>
 <div class ="page-box">
     <h2>Submit Review</h2>
     <form method="post" action="../controller/submit_review.php">
@@ -12,6 +13,12 @@
     <textarea name="review" placeholder="Write your review" required></textarea>
     <button type="submit">Submit Review</button>
 </form>
+<?php
+if(isset($_GET['success'])&& $_GET['success']==1)
+    {
+        echo "<p class='message'>Review submitted successfully!</p>";
+    }
+    ?>
 </div>
 </body>
 </html>
